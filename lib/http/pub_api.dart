953c12d2c8d8e9b9694893d7e8c1481319292e68
zmtzawqlp <zmtzawqlp@live.com>
2020-10-22 01:09:11 +0800

    update api

--- lib/http/pub_api.dart
+++ lib/http/pub_api.dart
@@ -1,4 +1,5 @@
 import 'dart:convert' as json;
+import 'package:flutter/foundation.dart';
 import 'package:flutter_candies_gallery/common/util.dart';
 import 'package:flutter_candies_gallery/model/package_info.dart';
 import 'package:flutter_candies_gallery/model/package_metrics.dart';
@@ -8,6 +9,9 @@ import 'package:http/http.dart' as http;
 String get pubApiBaseUrl =>
     isCN ? 'https://pub.flutter-io.cn' : 'https://pub.dev';
 
+const String webPubApiBaseUrl= 'http://182.92.11.78:81/pub.php';
+
+
 class PubApi {
   factory PubApi() => _pubApi;
   PubApi._();
@@ -15,7 +19,7 @@ class PubApi {
 
   Future<PackageInfo> getInfo(String name) async {
     final http.Response response =
-        await http.get('$pubApiBaseUrl/api/packages/$name');
+        await http.get('${kIsWeb?webPubApiBaseUrl:pubApiBaseUrl}/api/packages/$name');
 
     return PackageInfo.fromJson(
         json.jsonDecode(response.body) as Map<String, dynamic>);
@@ -23,7 +27,7 @@ class PubApi {
 
   Future<PackageScore> getScore(String name) async {
     final http.Response response =
-        await http.get('$pubApiBaseUrl/api/packages/$name/score');
+        await http.get('${kIsWeb?webPubApiBaseUrl:pubApiBaseUrl}/api/packages/$name/score');
 
     return PackageScore.fromJson(
         json.jsonDecode(response.body) as Map<String, dynamic>);
@@ -31,7 +35,7 @@ class PubApi {
 
   Future<PackageMetrics> getSMetrics(String name) async {
     final http.Response response =
-        await http.get('$pubApiBaseUrl/api/packages/$name/metrics');
+        await http.get('${kIsWeb?webPubApiBaseUrl:pubApiBaseUrl}/api/packages/$name/metrics');
 
     return PackageMetrics.fromJson(
         json.jsonDecode(response.body) as Map<String, dynamic>);
